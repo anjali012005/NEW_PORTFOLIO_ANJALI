73f57ccfc2f0617c36ec3661515b46e79b7919df
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-30 21:53:04 +0530

    Created MyWork Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
+import MyWork from './Components/MyWork/MyWork'
 
 //1:09min
 
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About/>
       <Services/>
+      <MyWork/>
     </div>
   )
 }
---
+++ src/assets/mywork_data.js
@@ -0,0 +1,38 @@
+import project1_img from '../assets/project1.png'
+import project2_img from '../assets/project2.png'
+import project3_img from '../assets/project3.png'
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "Full-Stack Web Development",
+    w_img: project1_img,
+  },
+  {
+    w_no: 2,
+    w_name: "React Projects (Frontend)",
+    w_img: project2_img,
+  },
+  {
+    w_no: 3,
+    w_name: "Node.js + Express (Backend APIs)",
+    w_img: project3_img,
+  },
+  {
+    w_no: 4,
+    w_name: "MongoDB Database Design",
+    w_img: project1_img,
+  },
+  {
+    w_no: 5,
+    w_name: "YouTube Video Summarizer App",
+    w_img: project2_img,
+  },
+  {
+    w_no: 6,
+    w_name: "Social Web App (MERN Stack)",
+    w_img: project3_img,
+  }
+];
+
+export default mywork_data;
