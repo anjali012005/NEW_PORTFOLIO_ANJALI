88c30d58bef2c41c4440e302ff910207623dc676
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-07-17 13:43:16 +0530

    Integrated contact form with email

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,28 @@ import { CiLocationOn } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
 
 const Contact = () => {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b532050d-7e03-4d4f-bbc0-d6e0a84b3487");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      alert(res.message);
+    }
+  };
   return (
     <div id="contact" className='contact'>
       <div className="contact-title">
@@ -26,7 +48,7 @@ const Contact = () => {
                 </div>
             </div>
         </div>
-        <form className='contact-right'>
+        <form onSubmit={onSubmit} className='contact-right'>
             <label htmlFor="">Your Name</label>
             <input type="text" placeholder="Enter your name" name="name" />
             <label htmlFor="">Your Email</label>
